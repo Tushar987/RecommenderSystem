707643e35c6e1908b051f8247eb365e246d8d50f
Tushar <tushar987gupta@gmail.com>
2017-11-14 02:53:04 +0530

    Added some Exploratory Analysis

--- README.md
+++ README.md
@@ -28,6 +28,8 @@ Two ways for calculating similarity scores
 
 ### ***Citation***
 ***
-The MovieLens Dataset :
+**The MovieLens Dataset** :
+
+MovieLens is a research site run by GroupLens Research at the University of Minnesota.
 > F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History and Context. ACM Transactions on Interactive Intelligent Systems (TiiS) 5, 4, Article 19 (December 2015), 19 pages. DOI=<http://dx.doi.org/10.1145/2827872>
 
--- Recommender-System-Python.py
+++ Recommender-System-Python.py
@@ -14,31 +14,123 @@
 # 
 # <hr style="height:2px">
 
-# In[ ]:
+# In[1]:
 
 # Import Packages
 
 from math import sqrt
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Getting more than one output Line
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
 
-# In[ ]:
+# In[2]:
 
 # Getting the Dataset
 
-df1= pd.read_csv("../input/movies.csv",usecols=['movieId','title']) # ,nrows=5000)
-df1.describe(include='all')
-df1.head()
+movies= pd.read_csv("./input/movies.csv") # ,nrows=5000)
+movies.head()
 
-df2=pd.read_csv("../input/ratings.csv",usecols=['userId','movieId','rating']) #,nrows=1000)
-df2.describe(include='all')
-df2.head()
+ratings=pd.read_csv("./input/ratings.csv",usecols=['userId','movieId','rating']) #,nrows=1000)
+ratings.head()
 
+tags= pd.read_csv('./input/tags.csv')
+tags.head()
+
+
+
+# In[3]:
+
+# Deleting unnecessary columns
+## for time being we will not consider the timestamp of movie for our analysis
+
+
+del tags['timestamp']
+
+
+# > ## **Descriptive Statistics**
+
+# In[4]:
+
+movies.info()
+
+
+# **Takeaway** : As we can see MovieIDs are some randomly generated number, so we will not be using this dataframe in our analysis. The only use for this table will be for Lookup of movie titles after the prediction.
+
+# In[5]:
+
+ratings['rating'].describe(include='all')
+
+
+# In[6]:
+
+ratings.groupby('rating')['movieId'].nunique()
+get_ipython().magic('matplotlib inline')
+ratings.hist(column='rating',figsize=(10,10),bins=5,grid=False)
+
+
+# * value of the ratings ranges from 0.5 to 5.0
+# * User tend to give 4 ratings movie that thay liked. They rarely give 1 star or less to a movie.
+# * **Future Plan**: (To decrease the complexity of model we can round ratings to their closest integer.)
+# 
+
+# In[7]:
+
+tag_counts = tags['tag'].value_counts()
+tag_counts[:15]
+tag_counts[:15].plot(kind='bar' , figsize=(10,10))
+
+
+# In[8]:
+
+movies['movieId'].count()
+
+
+# ### Analysis of different genres
+
+# #### Removing NaN values
+
+# In[9]:
+
+genre_filter= (movies['genres'] == '(no genres listed)')
+## Removing movies with no genre
+
+movies=movies[~genre_filter]
+movies=movies.reset_index(drop=True)  ## God bless StackOverflow.. Remeber removing filtered rows does not reindex the dataframe
+
+
+
+# #### Checking Total genres present in DataSet
+
+# In[10]:
+
+genres_count= {}
+for row in range(movies['movieId'].count()):
+    for genre in movies['genres'][row].split("|"):
+        if(genre != ''):
+            genres_count[genre]= genres_count.get(genre,0)+1
+        
+genres_count
+
+
+# In[11]:
+
+fig, ax = plt.subplots(figsize=(15,10))
+ax.barh(range(len(genres_count)),genres_count.values())
+plt.yticks(range(len(genres_count)),list(genres_count.keys()))
+plt.xlabel('Movie Count')
+plt.title("Genre Popularty")
+for i, v in enumerate(genres_count.values()):
+    ax.text(v + 20, i + .10, v, color='blue', fontweight='bold')
+
+
+# ***Deduction*** :
+# * There are high number of movies from genre Drama & Comedy. So, they might create abias toward the movies which are  from these genres.
+# * Film- noir & IMAX are the least popular category for films
 
 # > ## **Euclidean Distance Score**
 # 
@@ -46,21 +138,21 @@ df2.head()
 # * distance = 1/(1+sqrt of sum of squares between two points)
 # * value varies between 0 to 1, where closeness to 1 implies higher similarity.****
 
-# In[ ]:
+# In[14]:
 
 def euclidean_distance(person1,person2):
     #Getting details of person1 and person2
-    df_first= df2.loc[df2.userId==person1]
-    df_second= df2.loc[df2.userId==person2]
+    df_first= ratings.loc[ratings['userId']==person1]
+    df_second= ratings.loc[ratings.userId==person2]
     
     #Finding Similar Movies for person1 & person2 
-    ratings= pd.merge(df_first,df_second,how='inner',on='movieId')
+    df= pd.merge(df_first,df_second,how='inner',on='movieId')
     
     #If no similar movie found, return 0 (No Similarity)
-    if(len(ratings)==0): return 0
+    if(len(df)==0): return 0
     
     #sum of squared difference between ratings
-    sum_of_squares=sum(pow((ratings['rating_x']-ratings['rating_y']),2))
+    sum_of_squares=sum(pow((df['rating_x']-df['rating_y']),2))
     return 1/(1+sum_of_squares)
     
 # Checking working by passing similar ID, Corerelation should be 1
@@ -75,31 +167,31 @@ euclidean_distance(1,1) # Swwweeettt!!!
 # 
 # Slightly better than Euclidean because it addresses the the situation where the data isn't normalised. Like a User is giving high movie ratings in comparison to AVERAGE user.
 
-# In[ ]:
+# In[16]:
 
 def pearson_score(person1,person2):
     
     #Get detail for Person1 and Person2
-    df_first= df2.loc[df2.userId==person1]
-    df_second= df2.loc[df2.userId==person2]
+    df_first= ratings.loc[ratings.userId==person1]
+    df_second= ratings.loc[ratings.userId==person2]
     
     # Getting mutually rated items    
-    ratings= pd.merge(df_first,df_second,how='inner',on='movieId')
+    df= pd.merge(df_first,df_second,how='inner',on='movieId')
     
     # If no rating in common
-    n=len(ratings)
+    n=len(df)
     if n==0: return 0
 
     #Adding up all the ratings
-    sum1=sum(ratings['rating_x'])
-    sum2=sum(ratings['rating_y'])
+    sum1=sum(df['rating_x'])
+    sum2=sum(df['rating_y'])
     
     ##Summing up squares of ratings
-    sum1_square= sum(pow(ratings['rating_x'],2))
-    sum2_square= sum(pow(ratings['rating_y'],2))
+    sum1_square= sum(pow(df['rating_x'],2))
+    sum2_square= sum(pow(df['rating_y'],2))
     
     # sum of products
-    product_sum= sum(ratings['rating_x']*ratings['rating_y'])
+    product_sum= sum(df['rating_x']*df['rating_y'])
     
     ## Calculating Pearson Score
     numerator= product_sum - (sum1*sum2/n)
@@ -116,12 +208,12 @@ pearson_score(1,1)
 
 # >## **Getting result based on Pearson Score**
 
-# In[ ]:
+# In[17]:
 
 # Returns the best matches for person from the prefs dictionary.
 # Number of results and similarity function are optional params.
 def topMatches(personId,n=5,similarity=pearson_score):
-    scores=[(similarity(personId,other),other) for other in df2.loc[df2['userId']!=personId]['userId']]
+    scores=[(similarity(personId,other),other) for other in ratings.loc[ratings['userId']!=personId]['userId']]
     # Sort the list so the highest scores appear at the top
     scores.sort( )
     scores.reverse( )
@@ -143,9 +235,9 @@ def getRecommnedation(personId, similarity=pearson_score):
     '''
     totals,simSums= {},{}
     
-    df_person= df2.loc[df2.userId==personId]
+    df_person= ratings.loc[ratings.userId==personId]
     
-    for otherId in df2.loc[df2['userId']!=personId]['userId']: # all the UserID except personID
+    for otherId in ratings.loc[ratings['userId']!=personId]['userId']: # all the UserID except personID
         
         # Getting Similarity with OtherID
         sim=similarity(personId,otherId)
@@ -153,7 +245,7 @@ def getRecommnedation(personId, similarity=pearson_score):
         # Ignores Score of Zero or Negatie correlation         
         if sim<=0: continue
             
-        df_other=df2.loc[df2.userId==otherId]
+        df_other=ratings.loc[ratings.userId==otherId]
         
         #Movies not seen by the personID
         movie=df_other[~df_other.isin(df_person).all(1)]
@@ -179,7 +271,7 @@ def getRecommnedation(personId, similarity=pearson_score):
         recommendedId=np.array([x[1] for x in ranking])
         
         
-        return np.array(df1[df1['movieId'].isin(recommendedId)]['title'])[:20]
+        return np.array(movies[movies['movieId'].isin(recommendedId)]['title'])[:20]
 
 
 # In[ ]:
